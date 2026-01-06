447dc2af8e9675c4eb0e174ffe928fe294b9b51c
lazKorsan <ahmeteldes2002@gmail.com>
2026-01-07 01:18:09 +0300

    runner eklendi

--- src/test/java/webTest/TestAutamationPractice/Runner/TestRunner.java
+++ src/test/java/webTest/TestAutamationPractice/Runner/TestRunner.java
@@ -1,4 +1,20 @@
 package webTest.TestAutamationPractice.Runner;
 
-public class TestRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/java/webTest/TestAutamationPractice/features",
+        glue = "webTest/TestAutamationPractice/Stepdefinitions",
+        tags = "@formDoldurma",
+        dryRun = false,
+        monochrome = true,
+        publish = true,
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/test-automation-practice.html",
+                "json:target/cucumber-reports/test-automation-practice.json"
+        }
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
